a5a91f3e8278b2b7b19edc2d2ed326a7e070e1c4
skearya <77034153+skearya@users.noreply.github.com>
2026-05-01 13:07:24 -0400

    upgrade pillow (avoids building package on 3.14)

--- streamrip/media/artwork.py
+++ streamrip/media/artwork.py
@@ -130,24 +130,23 @@ def downscale_image(input_image_path: str, max_dimension: int):
 
     """
     # Open the image
-    image = Image.open(input_image_path)
+    with Image.open(input_image_path) as image:
+        # Get the original width and height
+        width, height = image.size
 
-    # Get the original width and height
-    width, height = image.size
+        if max_dimension >= max(width, height):
+            return
 
-    if max_dimension >= max(width, height):
-        return
+        # Calculate the new dimensions while maintaining the aspect ratio
+        if width > height:
+            new_width = max_dimension
+            new_height = int(height * (max_dimension / width))
+        else:
+            new_height = max_dimension
+            new_width = int(width * (max_dimension / height))
 
-    # Calculate the new dimensions while maintaining the aspect ratio
-    if width > height:
-        new_width = max_dimension
-        new_height = int(height * (max_dimension / width))
-    else:
-        new_height = max_dimension
-        new_width = int(width * (max_dimension / height))
+        # Resize the image with the new dimensions
+        resized_image = image.resize((new_width, new_height))
 
-    # Resize the image with the new dimensions
-    resized_image = image.resize((new_width, new_height))
-
-    # Save the resized image
-    resized_image.save(input_image_path)
+        # Save the resized image
+        resized_image.save(input_image_path)
